0fb1298cbd4b69c5cac5d29949734068f53bde16
therese <tdiede@gmail.com>
2018-06-13 12:52:04 -0700

    add comments

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
 	region = "us-west-2"
 }
 
+# EC2 instance running web server on Ubuntu w/ security group
 resource "aws_instance" "example" {
 	ami = "ami-db710fa3"
 	instance_type = "t2.micro"
@@ -18,10 +19,12 @@ resource "aws_instance" "example" {
 	}
 }
 
+# publishes the public IP address in Terraform output
 output "public_ip" {
-	value = "${aws_instance.example.public_ip"
+	value = "${aws_instance.example.public_ip}"
 }
 
+# allows incoming requests on port 8080 from any IP
 resource "aws_security_group" "instance" {
 	name = "terraform-example-instance-sg"
 
@@ -33,6 +36,7 @@ resource "aws_security_group" "instance" {
 	}
 }
 
+# first step in creating an auto-scaling group (ASG)
 resource "aws_launch_configuration" "example" {
 	image_id = "ami-db710fa3"
 	instance_type = "t2.micro"
