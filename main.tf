fe6bae3c61fc5624e30f1adfb4eb4ffe168011db
therese <tdiede@gmail.com>
2018-06-13 12:45:54 -0700

    add security group and launch configuration

--- main.tf
+++ main.tf
@@ -5,14 +5,32 @@ provider "aws" {
 resource "aws_instance" "example" {
 	ami = "ami-db710fa3"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
 	user_data = <<-EOF
 				#!/bin/bash
-				echo "Hello, World" > index.html
+				echo "Hello, World (from Therese)" > index.html
 				nohup busybox httpd -f -p 8080 &
 				EOF
 
 	tags {
 		Name = "terraform-example"
 	}
+}
+
+resource "aws_security_group" "instance" {
+	name = "terraform-example-instance-sg"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-db710fa3"
+	instance_type = "t2.micro"
+	security_groups = ["${aws_security_group.instance.id}"]
 }
